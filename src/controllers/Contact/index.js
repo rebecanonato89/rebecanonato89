b37ae7877b1a47aaa71e894d162c46e151a749a4
Rebeca Nonato Domingos <rebecanonato89@gmail.com>
2024-07-08 11:59:22 -0300

    Update index.js

--- src/controllers/Contact/index.js
+++ src/controllers/Contact/index.js
@@ -14,15 +14,11 @@ export default class Contact extends React.Component {
                     <ContactDiv>
                         <h1>Mais sobre mim:</h1>
                         <p>Rebeca Nonato, 30 anos, casada.<br />
-                            Residente atual da cidade de Lavras-MG.<br /><br />
+                            Residente atual do Brasil.<br /><br />
                             Aberta a oportunidades: <b>Sim</b><br />
                             Aberta a mudança de local: <b>Sim</b><br />
                         </p><br />
                         <h1>Entre em contato:</h1>
-                        <p>
-                            <a href="tel:31994298913">+55 (31) 99429-8913</a>
-                        </p>
-
                         <p>
                             <a href="mailto:rebecanonato89@gmail.com">rebecanonato89@gmail.com</a>
                         </p>
