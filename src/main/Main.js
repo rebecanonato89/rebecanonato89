212f5fa037947b8fb78180b38c83ec20c98e4e45
rebecanonato89 <rebecanonato89@gmail.com>
2025-06-13 15:53:00 -0300

    .

--- src/main/Main.js
+++ src/main/Main.js
@@ -7,19 +7,15 @@ import Skills from '../controllers/Skills';
 import Projects from '../controllers/Projects';
 import Contact from '../controllers/Contact';
 
-
 function Main() {
-    return (
-        <>
-            <Header />
-            <Gretting />
-            <Skills />
-            <Projects />
-            <Contact />
-            <Footer />
-            <TopPage />
-        </>
-    );
-  }
+  return (
+    <>
+      <div className="maintenance">
+        <h1>Site em Manutenção</h1>
+        <p>Estamos realizando algumas melhorias. Voltaremos em breve!</p>
+      </div>
+    </>
+  );
+}
 
 export default Main;
